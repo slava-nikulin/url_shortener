f2e2025cbc0382a65ba97bdab1526d386b1e89d8
vnikulin <vnikulin@rnds.pro>
2017-11-13 17:45:13 +0300

    fix configs for production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,18 +1,26 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
+environment.plugins = environment.plugins || [];
+
 environment.plugins.set(
-  'Provide',
+  'jquery',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default'],
-  }),
+  })
+)
+
+// this is not working
+environment.plugins.set(
+  'prod',
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: '"production"'
     }
   })
 )
+
 module.exports = environment
